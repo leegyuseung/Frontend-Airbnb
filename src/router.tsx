b69a3d46e576cc5a50b6723a36915091ad497093
leegyuseung <gyus0421@gmail.com>
2024-11-27 17:32:21 +0900

    #17.4 Not Found Page

--- src/router.tsx
+++ src/router.tsx
@@ -1,16 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
 import Root from "./components/Root";
 import Home from "./routes/Home";
-import Users from "./routes/Users";
+import NotFound from "./routes/NotFound";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
-    children: [
-      { path: "", element: <Home /> },
-      { path: "users", element: <Users /> },
-    ],
+    children: [{ path: "", element: <Home /> }],
+    errorElement: <NotFound />,
   },
 ]);
 
