925723f6e441fc2991ec922baa66042e9c9d40de
leegyuseung <gyus0421@gmail.com>
2024-11-30 23:20:01 +0900

    #18.1 Log In Modal

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,8 +1,26 @@
-import { Box, Button, HStack } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  HStack,
+  IconButton,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
+  VStack,
+} from "@chakra-ui/react";
 import { Outlet, Link } from "react-router-dom";
-import { FaAirbnb } from "react-icons/fa6";
+import { FaAirbnb, FaMoon, FaUserAstronaut, FaLock } from "react-icons/fa6";
 
 export default function Root() {
+  const { isOpen, onClose, onOpen } = useDisclosure();
   return (
     <Box>
       <HStack
@@ -17,9 +35,38 @@ export default function Root() {
           </Link>
         </Box>
         <HStack spacing={2}>
-          <Button>Log in</Button>
+          <IconButton
+            variant={"ghost"}
+            aria-label="Toggle dark mode"
+            icon={<FaMoon />}
+          />
+          <Button onClick={onOpen}>Log in</Button>
           <Button colorScheme={"red"}>Sign up</Button>
         </HStack>
+        <Modal onClose={onClose} isOpen={isOpen}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Log in</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+              <VStack>
+                <InputGroup>
+                  <InputLeftElement children={<FaUserAstronaut />} />
+                  <Input placeholder="Username" variant={"filled"} />
+                </InputGroup>
+                <InputGroup>
+                  <InputLeftElement children={<FaLock />} />
+                  <Input placeholder="Password" variant={"filled"} />
+                </InputGroup>
+              </VStack>
+            </ModalBody>
+            <ModalFooter>
+              <Button colorScheme={"red"} width={"100%"}>
+                Log in
+              </Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
       </HStack>
       <Outlet />
     </Box>
