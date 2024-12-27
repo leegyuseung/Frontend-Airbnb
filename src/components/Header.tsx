c49548999e5e49225475d3540feda14e8ba24d34
leegyuseung <gyus0421@gmail.com>
2024-12-27 21:29:20 +0900

    #20.4 CSRF

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -20,6 +20,7 @@ import LoginModal from "./LoginModal";
 import SignUpModal from "./SignUpModal";
 import useUser from "../lib/useUser";
 import { logOut } from "../api";
+import { useQueryClient } from "@tanstack/react-query";
 
 export default function Header() {
   const { userLoading, isLoggedIn, user } = useUser();
@@ -38,15 +39,18 @@ export default function Header() {
   const logoColor = useColorModeValue("red.500", "red.200");
   const Icon = useColorModeValue(FaMoon, FaSun);
   const toast = useToast();
-  const onLogOut = async () => {
-    // const data = await logOut();
+  const queryClient = useQueryClient();
 
+  const onLogOut = async () => {
     const toastId = toast({
       title: "Login out...",
       description: "Sad to see you go...",
       status: "loading",
       position: "bottom-right",
     });
+
+    await logOut();
+    queryClient.refetchQueries(["me"]);
 
     toast.update(toastId, {
       status: "success",
