872d173df88f09bff75dabac0cece7d5bdeb41c2
leegyuseung <gyus0421@gmail.com>
2025-01-03 21:31:53 +0900

    #20.10 Kakao Talk Auth

--- src/components/SocialLogin.tsx
+++ src/components/SocialLogin.tsx
@@ -2,6 +2,12 @@ import { Box, Button, Divider, HStack, Text, VStack } from "@chakra-ui/react";
 import { FaComment, FaGithub } from "react-icons/fa6";
 
 export default function SocialLogin() {
+  const kakaoParams = {
+    client_id: "d2bc2d82990b07d32b9cb34d04cdf087",
+    redirect_uri: "http://127.0.0.1:3000/social/kakao",
+    response_type: "code",
+  };
+  const params = new URLSearchParams(kakaoParams).toString();
   return (
     <Box mb={4}>
       <HStack my={8}>
@@ -28,7 +34,13 @@ export default function SocialLogin() {
         >
           Continue with Github
         </Button>
-        <Button w={"100%"} leftIcon={<FaComment />} colorScheme={"yellow"}>
+        <Button
+          as={"a"}
+          href={`https://kauth.kakao.com/oauth/authorize?${params}`}
+          w={"100%"}
+          leftIcon={<FaComment />}
+          colorScheme={"yellow"}
+        >
           Continue with Kakao
         </Button>
       </VStack>
