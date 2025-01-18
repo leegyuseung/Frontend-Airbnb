feea74876ad69f33f4dddaad1e0306369fb098aa
leegyuseung <gyus0421@gmail.com>
2025-01-18 19:55:42 +0900

    #21.7 File Upload

--- src/routes/UploadPhotos.tsx
+++ src/routes/UploadPhotos.tsx
@@ -12,25 +12,37 @@ import { useForm } from "react-hook-form";
 import ProtectedPage from "../components/ProtectedPage";
 import useHostOnlyPage from "../components/HostOnlyPage";
 import { useMutation } from "@tanstack/react-query";
-import { getUploadURL } from "../api";
+import { getUploadURL, uploadImage } from "../api";
 
 interface IForm {
   file: FileList;
 }
 
+interface IUploadURLResponse {
+  id: string;
+  uploadURL: string;
+}
+
 export default function UploadPhotos() {
-  const { register, handleSubmit } = useForm<IForm>();
-  const mutation = useMutation(getUploadURL, {
+  const { register, handleSubmit, watch } = useForm<IForm>();
+  const uploadImageMutation = useMutation(uploadImage, {
     onSuccess: (data: any) => {
       console.log(data);
     },
   });
+  const uploadURLMutation = useMutation(getUploadURL, {
+    onSuccess: (data: IUploadURLResponse) => {
+      uploadImageMutation.mutate({
+        uploadURL: data.uploadURL,
+        file: watch("file"),
+      });
+    },
+  });
   const { roomPk } = useParams();
   useHostOnlyPage();
-  const onSubmit = (data: any) => {
-    mutation.mutate();
+  const onSubmit = () => {
+    uploadURLMutation.mutate();
   };
-
   return (
     <ProtectedPage>
       <Box
